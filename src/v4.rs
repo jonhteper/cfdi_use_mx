bd4b78715a1f0fcbd22248eb8240de03edf22ad5
jonhteper <jonhteper@protonmail.com>
2026-02-12 19:40:53 -0600

    refactor: hardcode CFDIUse's descriptions
    Remove CFDI_USE_*_TEXT constants and map descriptions directly in
    CFDIUse
    Hardcode Spanish descriptions and string conversions to simplify usage

--- src/v4.rs
+++ src/v4.rs
@@ -1,13 +1,6 @@
 use parse_display_derive::{Display, FromStr};
 
-use crate::{
-    CFDI_USE_D01_TEXT, CFDI_USE_D02_TEXT, CFDI_USE_D03_TEXT, CFDI_USE_D04_TEXT, CFDI_USE_D05_TEXT,
-    CFDI_USE_D06_TEXT, CFDI_USE_D07_TEXT, CFDI_USE_D08_TEXT, CFDI_USE_D09_TEXT, CFDI_USE_D10_TEXT,
-    CFDI_USE_G01_TEXT, CFDI_USE_G02_TEXT, CFDI_USE_G03_TEXT, CFDI_USE_I01_TEXT, CFDI_USE_I02_TEXT,
-    CFDI_USE_I03_TEXT, CFDI_USE_I04_TEXT, CFDI_USE_I05_TEXT, CFDI_USE_I06_TEXT, CFDI_USE_I07_TEXT,
-    CFDI_USE_I08_TEXT,
-};
-use std::{ops::Deref, str::FromStr};
+use std::str::FromStr;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Display, FromStr, Default, Hash)]
 pub enum CFDIUse {
@@ -46,31 +39,32 @@ impl From<&str> for CFDIUse {
 
 impl CFDIUse {
     pub fn description(&self) -> &'static str {
+        use CFDIUse::*;
         match self {
-            CFDIUse::G01 => CFDI_USE_G01_TEXT,
-            CFDIUse::G02 => CFDI_USE_G02_TEXT,
-            CFDIUse::G03 => CFDI_USE_G03_TEXT,
-            CFDIUse::I01 => CFDI_USE_I01_TEXT,
-            CFDIUse::I02 => CFDI_USE_I02_TEXT,
-            CFDIUse::I03 => CFDI_USE_I03_TEXT,
-            CFDIUse::I04 => CFDI_USE_I04_TEXT,
-            CFDIUse::I05 => CFDI_USE_I05_TEXT,
-            CFDIUse::I06 => CFDI_USE_I06_TEXT,
-            CFDIUse::I07 => CFDI_USE_I07_TEXT,
-            CFDIUse::I08 => CFDI_USE_I08_TEXT,
-            CFDIUse::D01 => CFDI_USE_D01_TEXT,
-            CFDIUse::D02 => CFDI_USE_D02_TEXT,
-            CFDIUse::D03 => CFDI_USE_D03_TEXT,
-            CFDIUse::D04 => CFDI_USE_D04_TEXT,
-            CFDIUse::D05 => CFDI_USE_D05_TEXT,
-            CFDIUse::D06 => CFDI_USE_D06_TEXT,
-            CFDIUse::D07 => CFDI_USE_D07_TEXT,
-            CFDIUse::D08 => CFDI_USE_D08_TEXT,
-            CFDIUse::D09 => CFDI_USE_D09_TEXT,
-            CFDIUse::D10 => CFDI_USE_D10_TEXT,
-            CFDIUse::CP01 => "Pagos",
-            CFDIUse::CN01 => "Nómina",
-            CFDIUse::S01 => "Sin efectos fiscales",
+            G01 => "Adquisición de mercancías",
+            G02 => "Devoluciones, descuentos o bonificaciones",
+            G03 => "Gastos en general",
+            I01 => "Construcciones",
+            I02 => "Mobiliario y equipo de oficina por inversiones",
+            I03 => "Equipo de transporte",
+            I04 => "Equipo de computo y accesorios",
+            I05 => "Dados, troqueles, moldes, matrices y herramental",
+            I06 => "Comunicaciones telefónicas",
+            I07 => "Comunicaciones satelitales",
+            I08 => "Otra maquinaria y equipo",
+            D01 => "Honorarios médicos, dentales y gastos hospitalarios",
+            D02 => "Gastos médicos por incapacidad o discapacidad",
+            D03 => "Gastos funerales",
+            D04 => "Donativos",
+            D05 => "Intereses reales efectivamente pagados por créditos hipotecarios (casa habitación)",
+            D06 => "Aportaciones voluntarias al SAR",
+            D07 => "Primas por seguros de gastos médicos",
+            D08 => "Gastos de transportación escolar obligatoria",
+            D09 => "Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones",
+            D10 => "Pagos por servicios educativos (colegiaturas)",
+            S01 => "Sin efectos fiscales",
+            CP01 => "Pagos",
+            CN01 => "Nómina",
         }
     }
 
@@ -105,31 +99,32 @@ impl CFDIUse {
     }
 
     pub fn from_description(str: &str) -> Self {
+        use CFDIUse::*;
         match str {
-            CFDI_USE_G01_TEXT => Self::G01,
-            CFDI_USE_G02_TEXT => Self::G02,
-            CFDI_USE_G03_TEXT => Self::G03,
-            CFDI_USE_I01_TEXT => Self::I01,
-            CFDI_USE_I02_TEXT => Self::I02,
-            CFDI_USE_I03_TEXT => Self::I03,
-            CFDI_USE_I04_TEXT => Self::I04,
-            CFDI_USE_I05_TEXT => Self::I05,
-            CFDI_USE_I06_TEXT => Self::I06,
-            CFDI_USE_I07_TEXT => Self::I07,
-            CFDI_USE_I08_TEXT => Self::I08,
-            CFDI_USE_D01_TEXT => Self::D01,
-            CFDI_USE_D02_TEXT => Self::D02,
-            CFDI_USE_D03_TEXT => Self::D03,
-            CFDI_USE_D04_TEXT => Self::D04,
-            CFDI_USE_D05_TEXT => Self::D05,
-            CFDI_USE_D06_TEXT => Self::D06,
-            CFDI_USE_D07_TEXT => Self::D07,
-            CFDI_USE_D08_TEXT => Self::D08,
-            CFDI_USE_D09_TEXT => Self::D09,
-            CFDI_USE_D10_TEXT => Self::D10,
-            "Pagos" => Self::CP01,
-            "Nómina" => Self::CN01,
-            "Sin efectos fiscales" => Self::S01,
+            "Adquisición de mercancías" => G01,
+            "Devoluciones, descuentos o bonificaciones" => G02,
+            "Gastos en general" => G03,
+            "Construcciones" => I01,
+            "Mobiliario y equipo de oficina por inversiones" => I02,
+            "Equipo de transporte" => I03,
+            "Equipo de computo y accesorios" => I04,
+            "Dados, troqueles, moldes, matrices y herramental" => I05,
+            "Comunicaciones telefónicas" => I06,
+            "Comunicaciones satelitales" => I07,
+            "Otra maquinaria y equipo" => I08,
+            "Honorarios médicos, dentales y gastos hospitalarios" => D01,
+            "Gastos médicos por incapacidad o discapacidad" => D02,
+            "Gastos funerales" => D03,
+            "Donativos" => D04,
+            "Intereses reales efectivamente pagados por créditos hipotecarios (casa habitación)" => D05,
+            "Aportaciones voluntarias al SAR" => D06,
+            "Primas por seguros de gastos médicos" => D07,
+            "Gastos de transportación escolar obligatoria" => D08,
+            "Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones" => D09,
+            "Pagos por servicios educativos (colegiaturas)" => D10,
+            "Sin efectos fiscales" => S01,
+            "Pagos" => CP01,
+            "Nómina" => CN01,
             _ => Self::default(),
         }
     }
